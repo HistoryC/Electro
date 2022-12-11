7fedea8bb7d6ae043f00e6fb7ad74bb9885a9cae
User <User@DESKTOP-KM4COFG>
2022-12-12 01:06:33 +0500

    Добавил описание к функциям.

--- src/main/java/electro/Calc.java
+++ src/main/java/electro/Calc.java
@@ -11,52 +11,63 @@ public abstract class Calc {
 	
 	/** The base tarif. */
 	private double baseTarif = 150;
+	/** The Window Cleaning tarif. */
 	private double windowCleaningTarif=0;
 	
 	/**
-	 * Mul.
-	 *
-	 * @param indications the indications
-	 * @param sum the sum
-	 * @return the double
+	 * перемножение с переменной baseTarif
+	 * @param value - размер квартиры
+	 * @param sum
+	 * @return sum
 	 */
 	public double sumBaseTarif(double value, double sum) {
 		sum+=baseTarif*value;
 		return sum;
 	}
-	
+	/**
+	 * сумма
+	 */
 	public void sumA() {	
 		
 	}
-	
+	/**
+	 * перемножение с переменной windowCleaningTarif
+	 * @param value - стоимость мойки окон
+	 * @param sum
+	 * @return sum
+	 */
 	public double sumWCTarif(double value, double sum) {
 		sum+=windowCleaningTarif*value;
 		return sum;
 	}
 	/**
-	 * Gets the base tarif.
-	 *
-	 * @return the base tarif
+	 * возвращает значение переменной baseTarif
+	 * @return baseTarif
 	 */
 	public double getBaseTarif() {
 		return baseTarif;	
 	}
 	
-	
+	/**
+	 * возвращает значение переменной windowCleaningTarif
+	 * @return windowCleaningTarif
+	 */
 	public double getWCTarif() {
 		return windowCleaningTarif;	
 	}
 	
 	/**
-	 * Sets the base tarif.
-	 *
-	 * @param tarif the new base tarif
+	 * устанавливает переменной baseTarif новое значение
+	 * @param tarif
 	 */
 	public void setBaseTarif(double tarif) {
 		this.baseTarif=tarif;
 	}
 	
-	
+	/**
+	 * устанавливает переменной windowCleaningTarif новое значение
+	 * @param tarif
+	 */
 	public void setWCTarif(double tarif) {
 		this.windowCleaningTarif=tarif;
 	}
